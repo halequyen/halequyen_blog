5c13e50a252b0c6db700eceb256df715449a2de9
thinnc <nguyenthin23102000@gmail.com>
2022-02-04 17:06:40 +0700

    update code: Login and logout

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,11 +1,12 @@
+const MiddlewareController = require('../app/controllers/MiddlewareController');
 const express = require('express');
 const router = express.Router();
 
 const newsController = require('../app/controllers/NewsController');
 
-router.get('/create', newsController.create);
+router.get('/create', MiddlewareController.verifyToken, newsController.create);
 router.post('/store', newsController.store);
-router.get('/:id/edit', newsController.edit);
+router.get('/:id/edit', MiddlewareController.verifyToken, newsController.edit);
 router.post('/handle-form-actions', newsController.handleFormActions);
 router.post(
     '/handle-trash-form-actions',
@@ -16,7 +17,7 @@ router.patch('/:id/restore', newsController.restore);
 router.delete('/:id', newsController.destroy);
 router.delete('/:id/force', newsController.forceDestroy);
 router.delete('/:id/forces', newsController.forcesDestroy);
-router.get('/:slug', newsController.show);
-router.get('/', newsController.index);
+router.get('/:slug', MiddlewareController.verifyToken, newsController.show);
+router.get('/', MiddlewareController.verifyToken, newsController.index);
 
 module.exports = router;
